a71b3820bbdb5286071758a9e5fb4196c42b8896
Shweta Shrinivas <shshri31@in.ibm.com>
2020-10-04 12:06:26 +0530

    Generated project on spring initializr

---
+++ src/main/java/com/importexport/tarfiles/TarfilesApplication.java
@@ -0,0 +1,13 @@
+package com.importexport.tarfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/importexport/tarfiles/TarfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.importexport.tarfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
