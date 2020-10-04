f699da06057086ad6c245bd38886a2e1694666d2
Shweta Shrinivas <shshri31@in.ibm.com>
2020-10-04 12:34:55 +0530

    Added domain/model class

--- src/main/java/com/importexport/tarfiles/model/TarFile.java
+++ src/main/java/com/importexport/tarfiles/model/TarFile.java
@@ -1,5 +1,49 @@
 package com.importexport.tarfiles.model;
 
+import org.bson.types.Binary;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "tar_files")
 public class TarFile {
 
+    @Id
+    private String id;
+    
+    private String fileTitle;
+        
+    private Binary file;
+
+	public TarFile(String id, String fileTitle, Binary file) {
+		super();
+		this.id = id;
+		this.fileTitle = fileTitle;
+		this.file = file;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFileTitle() {
+		return fileTitle;
+	}
+
+	public void setFileTitle(String fileTitle) {
+		this.fileTitle = fileTitle;
+	}
+
+	public Binary getFile() {
+		return file;
+	}
+
+	public void setFile(Binary file) {
+		this.file = file;
+	}
+    
+    
 }
