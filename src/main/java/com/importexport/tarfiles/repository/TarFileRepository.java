f1962ccc2b8908fcb238870d074c7a688bfe26d3
Shweta Shrinivas <shshri31@in.ibm.com>
2020-10-04 12:39:39 +0530

    Added TarFileRepository

--- src/main/java/com/importexport/tarfiles/repository/TarFileRepository.java
+++ src/main/java/com/importexport/tarfiles/repository/TarFileRepository.java
@@ -1,9 +1,11 @@
 package com.importexport.tarfiles.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.importexport.tarfiles.model.TarFile;
 
+@Repository
 public interface TarFileRepository extends MongoRepository<TarFile, String> { 
 	
 }
